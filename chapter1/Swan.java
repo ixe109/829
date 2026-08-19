365feb1825aae3e175d893f88f47f097bafc4958
ixe109 <isheanesumusengi@gmail.com>
2026-08-19 22:24:43 +0200

    bookmark update to listerals

---
+++ chapter1/Swan.java
@@ -0,0 +1,11 @@
+public class Swan {
+	int numberEggs;
+	
+	public static void main(String[] args){
+	Swan mother = new Swan();
+	mother.numberEggs = 1;
+
+	System.out.println("Number of Mother's Egggs: " + mother.numberEggs);
+	
+	}
+}

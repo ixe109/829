ed153ed176ebc33f6506b8731aa77a4cab5839d6
ixe109 <isheanesumusengi@gmail.com>
2026-08-20 22:29:49 +0200

    text block deep dive

---
+++ chapter1/Textblocks.java
@@ -0,0 +1,12 @@
+public class TextBlocks{
+	public static void main(String[] args){
+		String pyramid = """
+   "doe\"\""
+\"deer\"""
+""";
+		System.out.println(pyramid);
+		System.out.println("bbbbb\sbbbbbb");
+
+	}
+}
+
